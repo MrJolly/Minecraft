c1eccd1d177f4eb1e6c92091d63af63807a4144d
MrJolly <mr_jolly84@hotmail.com>
2015-07-29 08:28:36 +0100

    First set of tests

---
+++ First/src/main/java/net/ddns/mrjolly/test/help/Reference.java
@@ -0,0 +1,7 @@
+package net.ddns.mrjolly.test.help;
+
+public class Reference {
+	public static final String MODID = “test”; //must be lowercase
+	public static final String NAME = “Test”;
+	public static final String VERSION = “a0.1”;
+}
---
+++ First/src/main/java/net/ddns/mrjolly/test/test.java
@@ -0,0 +1,9 @@
+package net.ddns.mrjolly.test;
+
+import net.ddns.mrjolly.test.help.*;;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+
+public class test {
+
+}
